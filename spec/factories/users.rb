9b5f9fe8eb80293364e248b9391c8242985c5d7a
Derfler <rmderfler@gmail.com>
2014-09-14 15:58:15 -0700

    add factory girls

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    email 'email@email.com'
+    name 'Joe'
+    password 'password'
+  end
+end
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,6 +1,13 @@
+require 'rails_helper'
 describe User do
+
+  it 'works with factory girl' do
+    user = build(:user)
+    expect(user.email).to eq "email@email.com"
+    expect(user.name).to eq "Joe"
+  end
+
   it { should have_many(:friends).through(:friendships)}
   it { should validate_presence_of :name }
   it { should validate_uniqueness_of :email}
-
 end
