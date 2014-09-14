57f4836756db98a672228f8929defb7a76f48e8d
Derfler <rmderfler@gmail.com>
2014-09-14 14:28:26 -0700

    remove unused routes from user

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :views
   devise_for :users
-  resources :users
+  resources :users, only: [:index, :show, :new]
   resources :friendships, only: [:create, :destroy]
   root to: "users#index"
 end
